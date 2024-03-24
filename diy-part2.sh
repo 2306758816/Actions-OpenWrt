b5b360bdf3005c7f0e0a456073775d15b3ca7de7
tkop <2306758816@qq.com>
2024-03-24 18:01:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,12 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/Xiaomi/g' package/base-files/files/bin/config_generate
+
+sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
